19b667ecf252dc7f8900a057e1d3a45d3f43b8b6
Miki <ghermaico135@gmail.com>
2023-05-02 18:46:11 +0300

    Add awesome book class

--- awesomeBook.js
+++ awesomeBook.js
@@ -6,20 +6,79 @@ const author = document.querySelector("#author");
 const addBtn = document.querySelector("#addBtn");
 
 // book
+class Book {
+	constructor() {
+		this.userBook = [];
+	}
 
-class Books {
-	constructor(title, author) {
-		this.title = title;
-		this.author = author;
+	getBook() {
+		if (localStorage.getItem("userBook") === null) {
+			this.userBook = [];
+		} else {
+			this.userBook = JSON.parse(localStorage.getItem("userBook"));
+		}
+
+		return this.userBook;
+	}
+
+	setBook() {
+		localStorage.setItem("userBook", JSON.stringify(this.userBook));
+	}
+
+	addBooks(title, author) {
+		this.userBook.push({
+			id: Math.floor(Math.random() * 1000),
+			title,
+			author,
+		});
+	}
+
+	showList() {
+		let content = "";
+		this.userBook.forEach((book) => {
+			content += `
+    <div>
+          <p class="title">${book.bookTitle}</p>
+          <p class="author">${book.bookAuthor}</p><br>
+          <button type="submit" class="removeBtn" id="${book.id}">Remove</button>
+          <br>
+    </div>
+    `;
+		});
+
+		bookList.innerHTML = content;
+	}
+
+	remove(id) {
+		this.userBook = this.userBook.filter((book) => book.id.toString() !== id);
+	}
+
+	static clear(title, author) {
+		title.value = "";
+		author.value = "";
 	}
 }
-class BookList {
-	contructor() {
-		this.books = [];
+
+const bookStore = new Book();
+bookStore.getBook();
+bookStore.showList();
+
+// remove
+document.addEventListener("click", (e) => {
+	if (e.target.classList.contains("removeBtn")) {
+		bookStore.remove(e.target.id);
+		bookStore.setBook();
+		bookStore.showList();
 	}
-}
+});
 
+//Add EventListner
+addBtn.addEventListener("click", (e) => {
+	e.preventDefault();
+	console.log("pages");
 
-// userInterface
-
-// Eventlistners
+	// if(title.value === '' )
+	bookStore.addBooks(title.value, author.value);
+	bookStore.showList();
+	// bookStore.clear(title.value, author.value);
+});

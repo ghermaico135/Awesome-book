bdc8dd237406649e0672a56b52201f4fa77b3c5c
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-05-02 19:59:45 +0330

    Fix class issues

--- awesomeBook.js
+++ awesomeBook.js
@@ -1,84 +1,79 @@
 /** @format */
 
-const bookList = document.querySelector(".booklist");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const addBtn = document.querySelector("#addBtn");
+const bookList = document.querySelector('.booklist');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBtn = document.querySelector('#addBtn');
 
 // book
 class Book {
-	constructor() {
-		this.userBook = [];
-	}
+  constructor() {
+    this.userBook = [];
+  }
 
-	getBook() {
-		if (localStorage.getItem("userBook") === null) {
-			this.userBook = [];
-		} else {
-			this.userBook = JSON.parse(localStorage.getItem("userBook"));
-		}
+  // Get data from localstorage
+  getBook() {
+    this.userBook = JSON.parse(localStorage.getItem('userBook')) || [];
+  }
 
-		return this.userBook;
-	}
+  // set data to localstorage
+  setBook() {
+    localStorage.setItem('userBook', JSON.stringify(this.userBook));
+  }
 
-	setBook() {
-		localStorage.setItem("userBook", JSON.stringify(this.userBook));
-	}
+  addBooks(title, author) {
+    this.userBook.push({
+      id: Math.floor(Math.random() * 1000),
+      title: title,
+      author: author,
+    });
+  }
 
-	addBooks(title, author) {
-		this.userBook.push({
-			id: Math.floor(Math.random() * 1000),
-			title,
-			author,
-		});
-	}
-
-	showList() {
-		let content = "";
-		this.userBook.forEach((book) => {
-			content += `
+  showList() {
+    let content = '';
+    this.userBook.forEach((book) => {
+      content += `
     <div>
-          <p class="title">${book.bookTitle}</p>
-          <p class="author">${book.bookAuthor}</p><br>
+          <p class="title">${book.title}</p>
+          <p class="author">${book.author}</p><br>
           <button type="submit" class="removeBtn" id="${book.id}">Remove</button>
           <br>
     </div>
     `;
-		});
+    });
 
-		bookList.innerHTML = content;
-	}
+    return content;
+  }
 
-	remove(id) {
-		this.userBook = this.userBook.filter((book) => book.id.toString() !== id);
-	}
+  remove(id) {
+    this.userBook = this.userBook.filter((book) => book.id.toString() !== id);
+  }
 
-	static clear(title, author) {
-		title.value = "";
-		author.value = "";
-	}
+  static clear(title, author) {
+    title.value = '';
+    author.value = '';
+  }
 }
 
 const bookStore = new Book();
 bookStore.getBook();
 bookStore.showList();
 
-// remove
-document.addEventListener("click", (e) => {
-	if (e.target.classList.contains("removeBtn")) {
-		bookStore.remove(e.target.id);
-		bookStore.setBook();
-		bookStore.showList();
-	}
+//Add EventListner
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  bookStore.addBooks(title.value, author.value);
+  bookStore.setBook();
+  bookStore.getBook();
+  console.log(bookStore.showList());
+  bookList.innerHTML = bookStore.showList();
 });
 
-//Add EventListner
-addBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	console.log("pages");
-
-	// if(title.value === '' )
-	bookStore.addBooks(title.value, author.value);
-	bookStore.showList();
-	// bookStore.clear(title.value, author.value);
+// remove
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('removeBtn')) {
+    bookStore.remove(e.target.id);
+    bookStore.setBook();
+    bookList.innerHTML = bookStore.showList();
+  }
 });

906cd08cb2ff8b00d390686d062ac4948b33a862
Miki <ghermaico135@gmail.com>
2023-05-03 18:30:38 +0300

    Fixing linter error

--- awesomeBook.js
+++ awesomeBook.js
@@ -1,46 +1,46 @@
 /** @format */
 
-const bookList = document.querySelector(".booklist");
-const title = document.querySelector("#title");
-const author = document.querySelector("#author");
-const addBtn = document.querySelector("#addBtn");
-const showBookList = document.querySelector("#showBookList");
-const addBookList = document.querySelector("#addNewBook");
-const contact = document.querySelector("#contact");
-const showBook = document.querySelector("#showBook");
-const addBooks = document.querySelector("#addBook");
-const showContact = document.querySelector("#showContact");
-const currentDate = document.querySelector("#current-date");
+const bookList = document.querySelector('.booklist');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBtn = document.querySelector('#addBtn');
+const showBookList = document.querySelector('#showBookList');
+const addBookList = document.querySelector('#addNewBook');
+const contact = document.querySelector('#contact');
+const showBook = document.querySelector('#showBook');
+const addBooks = document.querySelector('#addBook');
+const showContact = document.querySelector('#showContact');
+const currentDate = document.querySelector('#current-date');
 
 // book
 class Book {
-	constructor() {
-		this.userBook = [];
-		this.date = new Date();
-	}
+  constructor() {
+    this.userBook = [];
+    this.date = new Date();
+  }
 
-	// Get data from localstorage
-	getBook() {
-		this.userBook = JSON.parse(localStorage.getItem("userBook")) || [];
-	}
+  // Get data from localstorage
+  getBook() {
+    this.userBook = JSON.parse(localStorage.getItem('userBook')) || [];
+  }
 
-	// set data to localstorage
-	setBook() {
-		localStorage.setItem("userBook", JSON.stringify(this.userBook));
-	}
+  // set data to localstorage
+  setBook() {
+    localStorage.setItem('userBook', JSON.stringify(this.userBook));
+  }
 
-	addBooks(title, author) {
-		this.userBook.push({
-			id: Math.floor(Math.random() * 1000),
-			title,
-			author,
-		});
-	}
+  addBooks(title, author) {
+    this.userBook.push({
+      id: Math.floor(Math.random() * 1000),
+      title,
+      author,
+    });
+  }
 
-	showList() {
-		let content = "";
-		this.userBook.forEach((book) => {
-			content += `
+  showList() {
+    let content = '';
+    this.userBook.forEach((book) => {
+      content += `
 <div
 class="container bg-light d-flex justify-content-between border border-dark p-2">
 <div>
@@ -53,28 +53,29 @@ Remove
 </div>
 </div>
     `;
-		});
+    });
 
-		return content;
-	}
+    return content;
+  }
 
-	remove(id) {
-		this.userBook = this.userBook.filter((book) => book.id.toString() !== id);
-	}
-	// show data
-	showDate() {
-		const d = new Intl.DateTimeFormat("en-us", {
-			dateStyle: "full",
-			timeStyle: "short",
-		});
-		let content = "";
-		content = `
+  remove(id) {
+    this.userBook = this.userBook.filter((book) => book.id.toString() !== id);
+  }
+
+  // show data
+  showDate() {
+    const d = new Intl.DateTimeFormat('en-us', {
+      dateStyle: 'full',
+      timeStyle: 'short',
+    });
+    let content = '';
+    content = `
 <div id="current-date" class="p-2 float-end">
 <h3 class="text-dark">${d.format(this.date)}</h3>
 </div>
 `;
-		return content;
-	}
+    return content;
+  }
 }
 
 const bookStore = new Book();
@@ -82,51 +83,51 @@ bookStore.getBook();
 bookStore.showList();
 
 // Add EventListner
-addBtn.addEventListener("click", (e) => {
-	e.preventDefault();
-	if (title.value === "" && author.value === "") {
-		alert("Please fill up all fields");
-	}
-	bookStore.addBooks(title.value, author.value);
-	bookStore.setBook();
-	bookStore.getBook();
-	bookList.innerHTML = bookStore.showList();
-	title.value = "";
-	author.value = "";
+addBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (title.value === '' && author.value === '') {
+    alert('Please fill up all fields');
+  }
+  bookStore.addBooks(title.value, author.value);
+  bookStore.setBook();
+  bookStore.getBook();
+  bookList.innerHTML = bookStore.showList();
+  title.value = '';
+  author.value = '';
 });
 
 // getting items to UI at loading
-window.addEventListener("load", () => {
-	bookList.innerHTML = bookStore.showList();
+window.addEventListener('load', () => {
+  bookList.innerHTML = bookStore.showList();
 });
 
 // remove
-document.addEventListener("click", (e) => {
-	if (e.target.classList.contains("removeBtn")) {
-		bookStore.remove(e.target.id);
-		bookStore.setBook();
-		bookList.innerHTML = bookStore.showList();
-	}
+document.addEventListener('click', (e) => {
+  if (e.target.classList.contains('removeBtn')) {
+    bookStore.remove(e.target.id);
+    bookStore.setBook();
+    bookList.innerHTML = bookStore.showList();
+  }
 });
 
-showBookList.addEventListener("click", () => {
-	showBook.style.visibility = "visible";
-	addBooks.style.visibility = "hidden";
-	showContact.style.visibility = "hidden";
+showBookList.addEventListener('click', () => {
+  showBook.style.visibility = 'visible';
+  addBooks.style.visibility = 'hidden';
+  showContact.style.visibility = 'hidden';
 });
 
-addBookList.addEventListener("click", () => {
-	showBook.style.visibility = "hidden";
-	addBooks.style.visibility = "visible";
-	showContact.style.visibility = "hidden";
+addBookList.addEventListener('click', () => {
+  showBook.style.visibility = 'hidden';
+  addBooks.style.visibility = 'visible';
+  showContact.style.visibility = 'hidden';
 });
 
-contact.addEventListener("click", () => {
-	showBook.style.visibility = "hidden";
-	addBooks.style.visibility = "hidden";
-	showContact.style.visibility = "visible";
+contact.addEventListener('click', () => {
+  showBook.style.visibility = 'hidden';
+  addBooks.style.visibility = 'hidden';
+  showContact.style.visibility = 'visible';
 });
 
-window.addEventListener("load", () => {
-	currentDate.innerHTML = bookStore.showDate();
+window.addEventListener('load', () => {
+  currentDate.innerHTML = bookStore.showDate();
 });
